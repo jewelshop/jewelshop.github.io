7e8af360af3a97f705467bc69de6d0ca96d1f018
renzaspiras <aspirasrenz@gmail.com>
2023-01-30 03:08:17 +0800

    I Add some built in modules!

--- js/script.js
+++ js/script.js
@@ -1,21 +1,104 @@
-/*fetch('data/object.json')
-  .then(response => response.json())
-  .then(data => {
-    roar(data)
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+//Lazy Script
+function make(elementType) {
+    return {
+      from: function(id) {
+        this.id = id;
+        return this;
+      },
+      injectTo: function(parentElement) {
+        this.parentElement = parentElement;
+        return this;
+      },
+      content: function(content) {
+        this.content = content;
+        return this;
+      },
+      create: function() {
+        let newElement = document.createElement(elementType);
+        newElement.id = this.id;
+        newElement.innerHTML = this.content;
+        let parent = document.querySelector(this.parentElement);
+        if (!parent) {
+            console.log("Parent element not found!");
+            return;
+        }
+        parent.appendChild(newElement);
+      }
+    }
+}
+function id(name) {
+    let element = document.getElementById(name);
+    return {
+      AddClass: function(className) {
+        element.classList.add(className);
+      },
+      RemoveClass: function(className) {
+        element.classList.remove(className);
+      },
+      ToggleClass: function(className) {
+        element.classList.toggle(className);
+      }
+    }
+}
+function img(src) {
+    return {
+        id: function(id) {
+            this.id = id;
+            return this;
+        },
+        alt: function(alt) {
+            this.alt = alt;
+            return this;
+        },
+        to: function(parentElement) {
+            let newImg = document.createElement("img");
+            newImg.src = src;
+            newImg.id = this.id;
+            newImg.alt = this.alt;
+            let parent = document.querySelector(parentElement);
+            if (!parent) {
+                console.log("Parent element not found!");
+                return;
+            }
+            parent.appendChild(newImg);
+        }
+    }
+}
+
+class uni_vars{
+    shopname = "Shop Name"
+    pages = ["Home", "Cart", "Account"]
+    book = this.pages.length
+    title = document.title
+}
+let value = new uni_vars()
+constructor()
+function constructor(){
+    interface()
+    navBar()
+}
 
 
-function roar(data){
-  console.log(data["hello"])
-}*/
-function onSignIn(googleUser) {
-  // Get the ID token and basic profile information
-  var id_token = googleUser.getAuthResponse().id_token;
-  var profile = googleUser.getBasicProfile();
 
-  // Send the ID token to your server for verification and authentication
-  // ...
+function interface(){    
+    make("section").from("header").injectTo("body").content("").create()
+
+    //Layer A000
+    make("div").from("layerA000").injectTo("#header").content("").create()
+    img("assets/img/logo.png").alt("Nothing").id("logo").to("#layerA000")
+    make("h3").from("headText").injectTo("#layerA000").content(value.shopname).create()
+
+    //Layer Nav
+    make("div").from("Nav").injectTo("#header").content("").create()
+}
+
+function navBar(){   
+     make("ul").from("navlist").injectTo("#Nav").content("").create()
+    for(i = 0; i < value.book; i++){
+        make("li").from(value.pages[i]).injectTo("#navlist").content(value.pages[i]).create()
+        id(value.pages[i]).AddClass("navItem")
+        if (value.pages[i] == value.title) {
+            id(value.pages[i]).AddClass("pageLight")
+        }
+    }
 }
