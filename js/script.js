cea84ce91ce458ee80d8220d9f18618b963a1d86
renzaspiras <aspirasrenz@gmail.com>
2023-01-29 20:55:12 +0800

    testing

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-fetch("data/hello.txt")
-  .then(response => response.text())
-  .then(data => console.log(data))
+/*fetch('data/object.json')
+  .then(response => response.json())
+  .then(data => {
+    roar(data)
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
+function roar(data){
+  console.log(data["hello"])
+}*/
+function onSignIn(googleUser) {
+  // Get the ID token and basic profile information
+  var id_token = googleUser.getAuthResponse().id_token;
+  var profile = googleUser.getBasicProfile();
+
+  // Send the ID token to your server for verification and authentication
+  // ...
+}
